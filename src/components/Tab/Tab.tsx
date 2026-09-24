12d756bda9275718cda46be68c24440085997028
Skoulskiy <ogurethcs@gmail.com>
2026-09-24 12:14:57 +0300

    feat: add drag and drop with dragging styles

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,13 +1,30 @@
 import styles from './Tab.module.scss';
+import cn from 'classnames';
 
 import type { TabInterface } from "../types/tab";
+import { useSortable } from '@dnd-kit/sortable';
+
+import { CSS } from '@dnd-kit/utilities';
 
 interface TabComponent {
   tab: TabInterface
 };
 
 export const Tab : React.FC<TabComponent> = ({ tab }) => {
-  return (<li className={styles['tab']}>
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id: tab.id});
+
+  const style = {
+    transform: CSS.Translate.toString(transform),
+    transition
+  };
+
+  return (<li 
+      className={cn(styles['tab'], { [styles['tab--dragging']]: isDragging })}
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
+    >
     <img src={tab.icon} alt={tab.label}/>
     <span>{tab.label}</span>
   </li>);
